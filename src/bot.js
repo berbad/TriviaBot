7427ec2e159387baae11c70f90fc689181fb0a47
Berdason Badel <bbadel@depaul.edu>
2021-09-25 19:16:26 -0500

    first commit

---
+++ src/bot.js
@@ -0,0 +1,218 @@
+require("dotenv").config();
+
+//console.log(process.version);
+
+const fs = require('fs');
+var path = require("path")
+var file = fs.readFileSync(path.resolve(__dirname, "./score.json"))
+
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] }); // creating an instance of the above class
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+const PREFIX = "!";
+
+const trivia = require("./questions"); // locating in src 
+var score = JSON.parse(file)
+
+
+client.on('ready', () => {
+    console.log('Login Successful');
+})
+
+// Welcome
+client.on('messageCreate', (message) => { // call back
+    if (message.author.bot) {
+        if(message.author.id === client.user.id) {
+            selfDestruct(message)
+        }
+        return;
+    } // ignores bot messages
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content === 'hello') {
+        message.reply('sup bitch');
+    }
+
+    if (message.content === PREFIX + 'trivia') {
+        message.reply('Select a Category: \nhistory \nassyriantrivia')
+    }
+
+    if (message.content === PREFIX +'trivia history') {
+        let rand = getRandomInt(trivia.history.length - 1);
+        let solution = trivia.history[rand].solution;
+        let answers = trivia.history[rand].answers;
+        let response = "";
+        //message.reply(trivia.history[rand].question);
+
+        for (let i = 0; i < answers.length; i++) {
+            response += solutionToEmoji(i) + " " + answers[i] + "\n"
+        }
+        message.reply("**"+trivia.history[rand].question+"**" + "\n" + response).then(function (message) {
+            message.react("1️⃣")
+            message.react("2️⃣")
+            message.react("3️⃣")
+            message.react("4️⃣")
+
+            const filter = (reaction, user) => {
+                return reaction.emoji.name === '1️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '2️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '3️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '4️⃣' && user.id != message.author.id;
+            };
+
+            message.awaitReactions({ filter, max: 20, time: 15000 })
+                .then(collected => {
+                    let ansReaction = collected.get(solutionToEmoji(trivia.history[rand].solution))
+                    if(ansReaction != null) {
+                        ansReaction.users.fetch().then(users => {
+                            users.each(user => {
+                                //console.log(user.id)
+                                //console.log(score[user.id])
+                                if(score[user.id] != null) {
+                                    score[user.id] = score[user.id] + 5;
+                                } else if (score[user.id] == null && user.id != "887021739505246208") {
+                                    score[user.id] = 5
+                                }
+                                
+                            })
+                            fs.writeFileSync(path.resolve(__dirname, "./score.json"), JSON.stringify(score, null, 2), 'utf8')
+                        })
+                    }
+                })
+                
+                .catch(e => {
+                    console.log(e)
+                    //console.log(`After 10 seconds, ${collected.size} player(s) reacted`);
+                });
+        });
+
+         setTimeout(() => {message.reply("Correct Answer: " + trivia.history[rand].answers[solution]);
+         } , 15000);
+    }
+
+    if (message.content === PREFIX +'trivia assyriantrivia') {
+        let rand = getRandomInt(trivia.assyriantrivia.length - 1);
+        let solution = trivia.assyriantrivia[rand].solution;
+        let answers = trivia.assyriantrivia[rand].answers;
+        let response = "";
+
+        for (let i = 0; i < answers.length; i++) {
+            response += solutionToEmoji(i) + " " + answers[i] + "\n"
+        }
+
+        message.reply("**"+trivia.assyriantrivia[rand].question +"**" + "\n" + response).then(function (message) {
+            message.react("1️⃣")
+            message.react("2️⃣")
+            message.react("3️⃣")
+            message.react("4️⃣")
+
+            const filter = (reaction, user) => {
+                return reaction.emoji.name === '1️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '2️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '3️⃣' && user.id != message.author.id
+                    || reaction.emoji.name === '4️⃣' && user.id != message.author.id;
+            };
+
+            message.awaitReactions({ filter, max: 20, time: 15000 })
+                .then(collected => {
+                    let ansReaction = collected.get(solutionToEmoji(trivia.assyriantrivia[rand].solution))
+                    if(ansReaction != null) {
+                        ansReaction.users.fetch().then(users => {
+                            users.each(user => {
+                                //console.log(user.id)
+                                //console.log(score[user.id])
+                                if(score[user.id] != null) {
+                                    score[user.id] = score[user.id] + 5;
+                                } else if (score[user.id] == null && user.id != "887021739505246208") {
+                                    score[user.id] = 5
+                                }
+                                
+                            })
+                            fs.writeFileSync(path.resolve(__dirname, "./score.json"), JSON.stringify(score, null, 2), 'utf8')
+                        })
+                    }
+                })
+                
+                .catch(e => {
+                    console.log(e)
+                    //console.log(`After 10 seconds, ${collected.size} player(s) reacted`);
+                });
+        });
+
+         setTimeout(() => {message.reply("Correct Answer: " + trivia.assyriantrivia[rand].answers[solution]);
+         } , 15000);
+    }
+
+    if (message.content === PREFIX + 'clearall') {
+        //message.channel.bulkDelete(100).catch(console.error);
+        message.channel.send(`mark delete it`).then(sentMessage => {
+            sentMessage.delete(5000);
+        });
+    }
+
+    if(message.content === PREFIX + 'leaderboard') {
+        leaderboard(client, message)
+    }
+});
+
+async function leaderboardHelper(client) {
+    let response = ""
+    for (let x in score) {
+        await client.users.fetch(x).then((User) => {
+            response += User.username + ": "+ score[x] + "\n"
+            //message.channel.send(User.username + ": "+ score[x])
+                //console.log(x + ": "+ score[x])
+            }).catch((err) => {
+                console.log(err)
+            })
+     }
+     return response
+}
+
+async function leaderboard(client, message) {
+    leaderboardHelper(client).then((response)=> {
+        message.channel.send(response)
+    }) 
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function emojiToSolution(emoji) {
+    if(emoji === "1️⃣") {
+        return 0;
+    }
+    else if(emoji === "2️⃣") {
+        return 1;
+    }
+    else if(emoji === "3️⃣") {
+        return 2;
+    }
+    else if(emoji === "4️⃣") {
+        return 3;
+    }
+    return -1;
+}
+
+function solutionToEmoji(solution) {
+    if(solution == 0) {
+        return "1️⃣";
+    }
+    else if(solution == 1) {
+        return "2️⃣";
+    }
+    else if(solution == 2) {
+        return "3️⃣";
+    }
+    else if(solution == 3) {
+        return "4️⃣";
+    }
+    return -1;
+}
+
+async function selfDestruct(message){
+    setTimeout(() => {
+        message.delete()
+         } , 40000);
+}
---
+++ src/questions.js
@@ -0,0 +1,240 @@
+module.exports=
+{
+    trivia: [
+        {
+            question: "q1",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 3
+        },
+        {
+            question: "q2",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 2
+        },
+        {
+            question: "q3",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 1
+        }
+    ],
+    ggames: [
+        {
+            question: "q1",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 3
+        },
+        {
+            question: "q2",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 2
+        },
+        {
+            question: "q3",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 1
+        }
+    ],
+    history: [
+        {
+            question: "Whose death sparked World War I?",
+            answers: ["Kaiser Wilhelm", "Archbishop Ussher", "Queen Victoria", "Archduke Franz Ferdinand"],
+            solution: 3
+        },
+        {
+            question: "Which of these nations was neutral in World War I?",
+            answers: ["Germany", "Norway", "Italy", "England"],
+            solution: 1
+        },
+        {
+            question: "Which of these ships was sunk by a German submarine?",
+            answers: ["Arizona", "Lusitania", "Titanic", "Andrea Doria"],
+            solution: 1
+        },
+        {
+            question: "Which weapon was first used at the Battle of the Somme in World War I?",
+            answers: ["Submarine", "Tank", "Jet fighter", "Chariot"],
+            solution: 1
+        },
+        {
+            question: "World War I ended in:",
+            answers: ["1925", "1918", "1920", "1915"],
+            solution: 1
+        },
+        {
+            question: "Which of these people was a spy in World War I?",
+            answers: ["James Bond", "Mata Hari", "Benedict Arnold", "Serge Plekhanov"],
+            solution: 1
+        },
+        {
+            question: "How many republics made up the former Soviet Union?",
+            answers: ["15", "12", "20", "10"],
+            solution: 0
+        },
+        {
+            question: "When was the first Nobel Prize in economics awarded?",
+            answers: ["1969", "1949", "1909", "1929"],
+            solution: 0
+        },
+        {
+            question: "Which book was written by Niccolò Machiavelli?",
+            answers: ["The Once and Future King", "The Prince", "The Good Earth", "War and Peace"],
+            solution: 1
+        },
+        {
+            question: "Of what country was Simón Bolívar president?",
+            answers: ["Bolivia", "Peru", "Argentina", "Chile"],
+            solution: 1
+        },
+        {
+            question: "Which Indian president was involved in the struggle for Irish independence?",
+            answers: ["V.V. Giri", "Neelam Sanjiva Reddy", "Gulzarilal Nanda", "S. Radhakrishnan"],
+            solution: 0
+        },
+        {
+            question: "Who was Karl Marx’s associate and fellow political theoretician?",
+            answers: ["Friedrich Nietzsche", "Friedrich II", "Friedrich Engels", "Friedrich Reich"],
+            solution: 2
+        },
+        {
+            question: "Where were the Aegean Bronze Age civilizations located?",
+            answers: ["Algeria", "India", "Greece", "Spain"],
+            solution: 2
+        },
+        {
+            question: "Which of these battles did not involve Roman soldiers?",
+            answers: ["Arretium", "Chalons", "Cannae", "Thermopylae"],
+            solution: 3
+        },
+        {
+            question: "Through which national park does the Continental Divide not pass?",
+            answers: ["Yellowstone", "Rocky Mountain", "Glacier", "Yosemite"],
+            solution: 3
+        },
+        {
+            question: "On what peninsula in Washington would you find the Olympic Mountains?",
+            answers: ["Seattle Peninsula", "Puget Peninsula", "Washington Peninsula", "Olympic Peninsula"],
+            solution: 3
+        },
+        {
+            question: "Who was the first U.S. president to appear on television?",
+            answers: ["Richard Nixon", "Ronald Reagan", "Abraham Lincoln", "Franklin Delano Roosevelt"],
+            solution: 3
+        },
+        {
+            question: "What automobile was named after Henry Ford’s only son?",
+            answers: ["Buick", "Oldsmobile", "Isuzu", "Edsel"],
+            solution: 3
+        },
+        {
+            question: "Which state seceded from Virginia in 1863?",
+            answers: ["the District of Columbia", "North Carolina", "West Virginia", "Maryland"],
+            solution: 2
+        },
+        {
+            question: "In what American state would you find Denali?",
+            answers: ["Alabama", "Arkansas", "Alaska", "Arizona"],
+            solution: 2
+        },
+        {
+            question: "Which modern states does the Natchez Trace join?",
+            answers: ["Mississippi and Alabama", "Mississippi and Arkansas", "Mississippi and Louisiana", "Mississippi and Tennessee"],
+            solution: 3
+        },
+        {
+            question: "What is the capital of Illinois?",
+            answers: ["Chicago", "Springfield", "Streator", "Urbana"],
+            solution: 1
+        }
+    ],
+    sports: [
+        {
+            question: "q1",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 3
+        },
+        {
+            question: "q2",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 2
+        },
+        {
+            question: "q3",
+            answers: ["a1", "a2", "a3", "a4"],
+            solution: 1
+        }
+    ],
+    assyriantrivia: [
+        {
+            question: "Where was the center of the Assyrian Empire?",
+            answers: ["Egypt", "Israel", "Northern Mesopotamia", "Southern Mesopotamia"],
+            solution: 2
+        },
+        {
+            question: "What other empire did the Assyrians battle throughout much of ancient history for control of Mesopotamia and the Middle East?",
+            answers: ["Akkadians", "Sumerians", "Babylonians", "Persians"],
+            solution: 2
+        },
+        {
+            question: "Who was the first king of the Assyrians to create an empire after the Akkadian Empire fell apart?",
+            answers: ["Sargon II", "Shamshi-Adad", "Hammurabi", "Ashurbanipal"],
+            solution: 1
+        },
+        {
+            question: "When the Assyrian Empire was at its peak, what lands did it control?",
+            answers: ["All of the Above", "Egypt", "Mesopotamia", "Israel"],
+            solution: 0
+        },
+        {
+            question: "Around what year did the last of the Assyrian Empires fall to the Babylonians?",
+            answers: ["2100 BC", "1074 BC", "740 BC", "612 BC"],
+            solution: 3
+        },
+        {
+            question: "What were the Assyrians most known for?",
+            answers: ["Their fearsome army and warriors", "Their poetry", "Their art and painting", "Their peaceful rule of the lands they conquered"],
+            solution: 0
+        }, 
+        {
+            question: "What Assyrian city held a famous library of thousands of clay tablets?",
+            answers: ["Babylon", "Nineveh", "Ashur", "Nimrud"],
+            solution: 1
+        },
+        {
+            question: "What metal did the Assyrians use to make their weapons, chariots, and tools strong?",
+            answers: ["Bronze", "Gold", "Iron", "Steel"],
+            solution: 2
+        },
+        {
+            question: "Who was the last great King of the Assyrians?",
+            answers: ["Sargon II", "Shamshi-Adad", "Tiglath-Pileser III", "Ashurbanipal"],
+            solution: 3
+        },
+        {
+            question: "Who was the main god of the Assyrians?",
+            answers: ["Ashur", "Nimrud", "Shamash", "Marduk"],
+            solution: 0
+        },
+    ],
+    boodaleon: [
+        {
+            question: "am i ugly",
+            answers: ["no", "no", "no", "yes"],
+            solution: 3
+        },
+        {
+            question: "why is this category here?",
+            answers: ["gun to my head", "leon a gangsta", "forced", "i beat women"],
+            solution: 2
+        },
+        {
+            question: "category3",
+            answers: ["cat3a", "cat3b", "cat3c", "cat3d"],
+            solution: 2
+        },
+        {
+            question: "category4",
+            answers: ["cat4a", "cat4b", "cat4c", "cat4d"],
+            solution: 2
+        }
+    ]
+}
